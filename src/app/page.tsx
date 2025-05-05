aabea4136e8e92d97749a1d2b7363cc361e59fe4
Cristobal Bravo <cristobal.bravo1601@alumnos.ubiobio.cl>
2025-05-05 18:13:20 -0400

    Actualiza la estructura de la página principal para incluir un nuevo componente Carousel y reorganiza las secciones. Se corrige el texto del encabezado en el componente Countdown para mejorar la consistencia. Se agrega un título en el componente Carousel que presenta fotos de la boda.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,22 +6,39 @@ import Location from "@/components/Location";
 export default function Home() {
   return (
     <div className="h-screen snap-y snap-mandatory overflow-y-scroll scroll-smooth bg-crema">
-      <section id="title" className="snap-start h-screen flex items-center justify-center">
+      <section
+        id="title"
+        className="snap-start h-screen flex items-center justify-center"
+      >
         <MainTitle />
       </section>
 
-      <section id="countdown" className="snap-start h-screen flex items-center justify-center">
+      <section
+        id="countdown"
+        className="snap-start h-screen flex items-center justify-center"
+      >
         <Countdown />
       </section>
 
-      <section id="location" className="snap-start h-screen flex items-center justify-center">
-        <Location />
+      <section
+        id="carousel"
+        className="snap-start h-screen flex items-center justify-center"
+      >
+        <Carousel />
       </section>
 
-      <section id="footer" className="snap-start h-screen flex items-center justify-center">
+      <section
+        id="location"
+        className="snap-start h-screen flex items-center justify-center"
+      >
+        <Location />
+      </section>
+      <section
+        id="footer"
+        className="snap-start h-screen flex items-center justify-center"
+      >
         <Footer />
       </section>
     </div>
   );
 }
-
--- src/components/Countdown.tsx
+++ src/components/Countdown.tsx
@@ -26,7 +26,7 @@ const Countdown = () => {
     return (
         <div className="w-full mx-auto my-4 md:my-8">
             <div className="border-4 border-dorado rounded-xl p-4 md:p-8 bg-crema shadow-xl h-40 md:h-80 flex flex-col items-center justify-center">
-                <h1 className="text-xl md:text-3xl font-elegant text-dorado mb-2 md:mb-4">Cuenta Regresiva para nuestra boda</h1>
+                <h1 className="text-xl md:text-3xl font-elegant text-dorado mb-2 md:mb-4">Cuenta regresiva para nuestra boda</h1>
                 <p className="text-2xl md:text-4xl font-elegant text-gray-800">{timeLeft}</p>
             </div>
         </div>
